b90725cfc225108042f07661357e476b37d8b7ff
soida <adioskensunny@gmail.com>
2025-06-26 21:07:00 +0800

    add en version

--- README.md
+++
@@ -1,2 +0,0 @@
-# flashfin_Light2D
- a 2d light system on CocosCreater 3.8.x.
--- index.000b2.js
+++ index.000b2.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.f7e24.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
