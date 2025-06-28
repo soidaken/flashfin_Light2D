5ddc44dc706dab2f23a4e353550043abc228a0eb
soida <adioskensunny@gmail.com>
2025-06-28 23:38:58 +0800

    v 0.2.1

--- index.650fb.js
+++ index.650fb.js
@@ -1,4 +1,4 @@
-System.register(["./application.f7e24.js"], function (_export, _context) {
+System.register(["./application.9ad13.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
